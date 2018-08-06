d9b48089599065a75954d89439e8596a7933d6b1
neillboyd <nboyd@bdna.com>
2018-08-06 09:52:24 +0100

    Adding docker compose for k6 tests

--- performance/happy_path_perf.js
+++ performance/happy_path_perf.js
@@ -1,6 +1,9 @@
 import { check, group, sleep } from "k6";
 import http from "k6/http";
 
+let API_HOST = `${__ENV.API_HOST}`;
+let API_PORT = `${__ENV.API_PORT}`;
+
 export let options = {
     vus: 5,
     iterations: 2000
@@ -10,7 +13,7 @@ export let options = {
 export default function() {
 
     group ("single_api_call", function() {
-        let res = http.get("http://localhost:1200/animal/dog");
+        let res = http.get("http://" + API_HOST + ":" + API_PORT + "/animal/dog");
         check(res, {
         "is status 200": (r) => r.status === 200,
         "response time is < 100ms": (r) => r.timings.duration < 100
